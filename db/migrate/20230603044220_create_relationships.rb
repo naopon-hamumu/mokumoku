d6f6aa12fd1c9aa9133df3610a715477ff0a4a85
naopon-hamumu <115980030+naopon-hamumu@users.noreply.github.com>
2023-06-03 22:41:35 +0900

    Add: 他のユーザーのフォロー機能

---
+++ db/migrate/20230603044220_create_relationships.rb
@@ -0,0 +1,12 @@
+class CreateRelationships < ActiveRecord::Migration[6.1]
+  def change
+    create_table :relationships do |t|
+      t.references :follower, foreign_key: { to_table: :users }, null: false
+      t.references :followed, foreign_key: { to_table: :users }, null: false
+
+      t.timestamps
+    end
+
+    add_index :relationships, [:follower_id, :followed_id], unique: true
+  end
+end
